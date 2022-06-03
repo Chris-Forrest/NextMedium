aa4f6e9a2acab5b12dca6e24aa9fb4918b101934
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2022-06-03 15:45:45 -0400

    made a header component
    Made a components folder added a file for header and imported the medium logo

--- sanity-medium/pages/index.tsx
+++ sanity-medium/pages/index.tsx
@@ -1,18 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header';
+
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div >
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>This is Medium 2.0</h1>
+      <Header />
+      
     </div>
-  )
+  );
 }
 
 export default Home
