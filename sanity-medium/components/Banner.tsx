b4639633e1dd30bf7e18948b62ae15eac9bf603a
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2022-06-03 18:25:25 -0400

    completed banner
    completed styling the banner

--- sanity-medium/components/Banner.tsx
+++ sanity-medium/components/Banner.tsx
@@ -2,15 +2,23 @@
 
 export default function Banner() {
   return (
-    <div>
-        <div>
+    <div className="flex justify-between items-center bg-yellow-400
+     border-y border-black py-10  lg:py-0">
+        <div className="px-10 space-y-5">
             <h1 className="text-6xl max-w-xl font-serif">
-                Medium is a place to write, read and connect
+                <span className="underline decoration-black
+                 decoration-4 pr-4">Medium</span> 
+                is a place to write, read and connect
             </h1>
             <h2>It's easy and free to post your thinking on any topic and 
                 connect with millions of readers.
             </h2>
         </div>
+
+        <img className="hidden md:inline-flex h-32 lg:h-full"
+         src="https://accountabilitylab.org/wp-content
+         /uploads/2020/03/Medium-logo.png"/>
+
     </div>
   );
 }
--- sanity-medium/pages/index.tsx
+++ sanity-medium/pages/index.tsx
@@ -6,7 +6,7 @@ import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div >
+    <div className='max-w-7xl max-auto' >
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
