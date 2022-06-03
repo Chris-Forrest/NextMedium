6f3519c899641b12c5222b774b1e1c15b7923270
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2022-06-03 18:05:53 -0400

    made a banner file
    added a banner file and started basic styling

---
+++ sanity-medium/components/Banner.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Banner() {
+  return (
+    <div>
+        <div>
+            <h1 className="text-6xl max-w-xl font-serif">
+                Medium is a place to write, read and connect
+            </h1>
+            <h2>It's easy and free to post your thinking on any topic and 
+                connect with millions of readers.
+            </h2>
+        </div>
+    </div>
+  );
+}
--- sanity-medium/pages/index.tsx
+++ sanity-medium/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Banner from '../components/Banner';
 import Header from '../components/Header';
 
 
@@ -12,6 +13,8 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+
+      <Banner />
       
     </div>
   );
