a9a58bbd2f5c0a27a0671f781916ddcbf62efa22
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2022-06-03 18:28:47 -0400

    Made Posts component
    Made a file in components for posts made a simple export and imported the un-styled component to the index

--- sanity-medium/pages/index.tsx
+++ sanity-medium/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import Posts from '../components/Posts';
 
 
 const Home: NextPage = () => {
@@ -16,6 +17,7 @@ const Home: NextPage = () => {
 
       <Banner />
       
+      <Posts />
     </div>
   );
 }
