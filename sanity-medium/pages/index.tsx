29dcf1de8a12bd80149141f6eee53e68bf919e39
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2022-06-06 19:06:00 -0400

    return data from api
    made getserverSideProps call in index and set up posts and api call data structure

--- sanity-medium/pages/index.tsx
+++ sanity-medium/pages/index.tsx
@@ -1,11 +1,16 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
 import Posts from '../components/Posts';
+import { sanityClient, } from '../sanity';
+import { Post } from "../typeings";
 
+interface Props {
+  posts: [Post];
+}
 
-const Home: NextPage = () => {
+export default function Home({ posts }: Props) {
+  console.log(posts)
   return (
     <div className='max-w-7xl max-auto' >
       <Head>
@@ -22,4 +27,23 @@ const Home: NextPage = () => {
   );
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author-> {
+    name,
+    image
+  },
+  description,
+  mainImage,
+  slug
+  }`;
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
